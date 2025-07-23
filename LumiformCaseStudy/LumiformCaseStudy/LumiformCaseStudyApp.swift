9050d1317925ae52a8e717aaa75e7a35b33b9894
Aly Yakan <alyyakan@hotmail.com>
2025-07-23 15:43:41 +0300

    Setup Xcode project

---
+++ LumiformCaseStudy/LumiformCaseStudy/LumiformCaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiformCaseStudyApp.swift
+//  LumiformCaseStudy
+//
+//  Created by Aly Yakan on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LumiformCaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
