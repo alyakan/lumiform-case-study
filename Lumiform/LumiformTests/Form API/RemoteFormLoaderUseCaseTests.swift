396af8a1e9fa14d4482516912914b1d80eb339c1
Aly Yakan <alyyakan@hotmail.com>
2025-07-24 19:16:37 +0300

    `RemoteFormLoader.load` send a request with the given URL to the http client

--- Lumiform/LumiformTests/Form API/RemoteFormLoaderUseCaseTests.swift
+++ Lumiform/LumiformTests/Form API/RemoteFormLoaderUseCaseTests.swift
@@ -9,22 +9,50 @@ import XCTest
 import Lumiform
 
 final class RemoteFormLoader {
+    private let url: URL
+    private let client: HTTPClient
 
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
 
+    func load(completion: @escaping (Result<Data, Error>) -> Void) {
+        client.get(from: url) { _ in }
     }
 }
 
-final class RemoteFormLoaderUseCaseTests: XCTest {
+final class RemoteFormLoaderUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFormLoader(client: client)
+        let (_, client) = makeSUT()
 
         XCTAssertEqual(client.requestedURLs, [], "Expected no requests, got: \(client.requestedURLs)")
     }
 
+    func test_load_requestsDataFromURL() {
+        let url = anyURL()
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load() { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+
     // MARK: - Helpers
+
+    private func makeSUT(
+        url: URL = URL(string: "https://some-url.com")!,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (RemoteFormLoader, HTTPClientSpy) {
+
+        let client = HTTPClientSpy()
+        let sut = RemoteFormLoader(url: url, client: client)
+        trackForMemoryLeaks(client)
+        trackForMemoryLeaks(sut)
+        return (sut, client)
+    }
 
     final class HTTPClientSpy: HTTPClient {
         private(set) var requestedURLs: [URL] = []
