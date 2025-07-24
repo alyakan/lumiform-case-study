afd90873a83783c3506e73dd1c1ca2aa6e7e0f98
Aly Yakan <alyyakan@hotmail.com>
2025-07-24 16:51:05 +0300

    Fix: Recreate project with Xcode 15.4 (#2)
    * Add the `Lumiform` macOS target which enables faster development and testing
    
    The code here should be agnostic of iOS/UI frameworks. By doing so, we eliminate the need to run a simulator whenever we need to test.
    
    * `URLSessionHTTPClient.get` performs request with given URL
    
    * Add a reusable method for tracking memory leaks by checking the given instance has been dealloacated after all tests are run
    
    * `URLSessionHTTPClient.get` returns error from failed requests
    
    * `URLSessionHTTPClient.get` returns data on successful HTTPURLResponse
    
    * Refactor the completion block by constructing a `Result` for more clarity
    
    * Create `HTTPClient` protocol as an abstraction for the `URLSessionHTTPClient`
    
    This allows us to replace the implementation easily in the future for better maintenance costs
    
    * Move `URLSessionHTTPClient` to production
    
    * Create folder for Form API in the tests target
    
    * Extract common test setup into helper methods for more brevity and clarity in tests
    
    * Move test helpers into their own files
    
    * Setup Github CI for running tests on pushes and pull requests
    
    * Fix typo in project path in the CI file
    
    * Set Xcode version to 15.3 so it can run on Github CI
    
    * Recreate project with Xcode 15.4 to enable Github CI from opening the project and running the tests
    
    * Fix xcodebuild command
    
    * Print available destinations in Github CI
    
    * Use arm64 instead of x86_64
    
    * Remove space
    
    * Fix project settings

---
+++ Lumiform/Lumiform/Lumiform.h
@@ -0,0 +1,18 @@
+//
+//  Lumiform.h
+//  Lumiform
+//
+//  Created by Aly Yakan on 24/07/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lumiform.
+FOUNDATION_EXPORT double LumiformVersionNumber;
+
+//! Project version string for Lumiform.
+FOUNDATION_EXPORT const unsigned char LumiformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lumiform/PublicHeader.h>
+
+
