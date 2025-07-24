5810acf2ec80c1f728304788e82bb188590d71f4
Aly Yakan <alyyakan@hotmail.com>
2025-07-24 16:51:56 +0300

    Revert "Fix: Recreate project with Xcode 15.4 (#2)"
    This reverts commit afd90873a83783c3506e73dd1c1ca2aa6e7e0f98.

---
+++ LumiformCaseStudy/LumiformCaseStudyTests/LumiformCaseStudyTests.swift
@@ -0,0 +1,13 @@
+//
+//  LumiformCaseStudyTests.swift
+//  LumiformCaseStudyTests
+//
+//  Created by Aly Yakan on 23/07/2025.
+//
+
+import XCTest
+import LumiformCaseStudy
+
+final class LumiformCaseStudyTests: XCTestCase {
+
+}
