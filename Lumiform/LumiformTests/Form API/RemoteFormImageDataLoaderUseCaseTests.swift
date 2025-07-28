10917b4e60b501c1e01a314346a34667a26ff4ed
Aly Yakan <alyyakan@hotmail.com>
2025-07-28 14:13:26 +0300

    `RemoteFormImageDataLoader.loadImageData` delivers connectivity and invalid response errors when appropriate

--- Lumiform/LumiformTests/Form API/RemoteFormImageDataLoaderUseCaseTests.swift
+++ Lumiform/LumiformTests/Form API/RemoteFormImageDataLoaderUseCaseTests.swift
@@ -17,12 +17,23 @@ protocol FormImageDataLoader {
 final class RemoteFormImageDataLoader: FormImageDataLoader {
     private let client: HTTPClient
 
+    enum Error: Swift.Error {
+        case connectivity, invalidResponse, invalidData
+    }
+
     init(client: HTTPClient) {
         self.client = client
     }
 
     func loadImageData(from url: URL, completion: @escaping (FormImageDataLoader.Result) -> Void) {
-        client.get(from: url) { _ in }
+        client.get(from: url) { result in
+            switch result {
+            case let .success((data, response)):
+                completion(.failure(Error.invalidResponse))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
     }
 }
 
@@ -43,6 +54,25 @@ class RemoteFormImageDataLoaderUseCaseTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
 
+    func test_loadImageDataFromURL_deliversConnectivityErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        expect(sut, toCompleteWith: failure(.connectivity), when: {
+            client.complete(with: anyNSError())
+        })
+    }
+
+    func test_loadImageDataFromURL_deliversInvalidResponseErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let samples = [199, 201, 300, 400, 500].enumerated()
+
+        samples.forEach { index, code in
+            expect(sut, toCompleteWith: failure(.invalidResponse), when: {
+                client.complete(withStatusCode: code, data: anyData(), at: index)
+            })
+        }
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FormImageDataLoader, client: HTTPClientSpy) {
@@ -51,5 +81,38 @@ class RemoteFormImageDataLoaderUseCaseTests: XCTestCase {
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, client)
+    }
+
+    private func expect(
+        _ sut: FormImageDataLoader,
+        toCompleteWith expectedResult: FormImageDataLoader.Result,
+        when action: () -> Void,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+
+        let url = anyURL()
+        let exp = expectation(description: "wait for completion")
+
+        sut.loadImageData(from: url) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        action()
+
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    private func failure(_ error: RemoteFormImageDataLoader.Error) -> FormImageDataLoader.Result {
+        .failure(error)
     }
 }
