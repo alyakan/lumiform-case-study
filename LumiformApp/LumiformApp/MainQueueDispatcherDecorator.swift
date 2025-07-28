0b8a9effa76ec361909e236a0f4129cf27a3afb8
Aly Yakan <alyyakan@hotmail.com>
2025-07-28 03:39:38 +0300

    Compose form loaders to enable caching

---
+++ LumiformApp/LumiformApp/MainQueueDispatcherDecorator.swift
@@ -0,0 +1,32 @@
+//
+//  MainQueueDispatcherDecorator.swift
+//  LumiformApp
+//
+//  Created by Aly Yakan on 28/07/2025.
+//
+
+import Lumiform
+
+final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { completion() }
+        }
+
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: FormLoader where T: FormLoader {
+    func load(completion: @escaping (FormLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
